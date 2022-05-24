9ff941631c22875327604630a6157c8a2eed9ecf
Justin <jjpeterson90@hotmail.com>
2022-05-24 17:07:06 -0500

    complete

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -4,6 +4,17 @@ for (let i = 1; i <= 1000; i++) {
 }
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
-
+    for (let i = 0; i < arrayToSearchThrough.length; i++) {
+        if (arrayToSearchThrough[i] === valueToFind) return i;
+        if (i === arrayToSearchThrough.length-1) return undefined;
+    }
 };
 
+exports.linearSearchGlobal = function (valueToFind, arrayToSearchThrough) {
+    let result = [];
+    for (let i = 0; i < arrayToSearchThrough.length; i++) {
+        if (arrayToSearchThrough[i] === valueToFind) result.push(i)
+        if (i === arrayToSearchThrough.length-1 && result === []) return undefined;
+        if (i === arrayToSearchThrough.length-1 && result !== []) return result;
+    }
+};
