55604abbee11144b0e4625aeeddf0a1f662c1b56
Justin <jjpeterson90@hotmail.com>
2022-05-25 14:31:58 -0500

    complete

--- python/linear_search.py
+++ python/linear_search.py
@@ -3,4 +3,15 @@ for number in range(1, 1001):
     array_to_search_through.append(number)
 
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
+    for i in range(len(array_to_search_through)):
+        if value_to_find == array_to_search_through[i]:
+            return i
+
+# print(linear_search(3, [1, 2, 3, 4, 5]))
+
+def global_linear_search(value_to_find, array_to_search_through):
+    result = []
+    for i in range(len(array_to_search_through)):
+        if value_to_find == array_to_search_through[i]:
+            result.append(i)
+    return result
